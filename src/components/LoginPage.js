f4e4527fb966922bb2d078b11ebafc7b2f32b418
Takayuki Honda <takka925H@gmail.com>
2021-08-24 23:15:49 +0900

    LoginPageコンポーネントを追加し、Sign upボタンとinput textを作成 要改良

---
+++ src/components/LoginPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const style = {
+    textAlign: "center"
+};
+
+const SignUpPage = () => {
+    return (
+        <div className="Login" style={style}>
+            <button className="buttonStyle">Sign up</button>
+        </div>
+    );
+}
+
+export default SignUpPage;

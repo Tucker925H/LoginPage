54c1d09e1f711550606e9b6f1af066e0ff407c22
Takayuki Honda <takka925H@gmail.com>
2021-08-26 14:56:41 +0900

    SignUpPageのstyleをを編集した。うまくいかないよー泣

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ import './App.css'
 
 function App() {
   return (
-    <div className="header">
+    <div className="signPage">
       <input 
         className="nickName"
         placeholder="mail-address"
       />
       <span className="focusLine"></span>
+      <input 
+        className="nickName"
+        placeholder="nickname"
+        />
+        <span className="focusLine"></span>
       <SignUpPage />
     </div>
   );
